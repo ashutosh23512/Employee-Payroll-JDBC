d88815ecc3f66306ef6b33b8f6ca9cbf658e2ec0
ASHUTOSH <ashutosh@192.168.1.6>
2020-10-28 15:19:55 +0530

    [Ashutosh]Created:JDBC

---
+++ src/main/java/com/dbdemo/DBDemo.java
@@ -0,0 +1,16 @@
+package com.dbdemo;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class DBDemo {
+
+}
---
+++ src/main/java/com/dbdemo/EmployeePayrollData.java
@@ -0,0 +1,5 @@
+package com.dbdemo;
+
+public class EmployeePayrollData {
+
+}
