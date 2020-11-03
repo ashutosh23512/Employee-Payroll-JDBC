7aabe4f6728984406744da2dc4f67fb4c043e95c
ASHUTOSH <ashutosh@192.168.1.6>
2020-11-03 15:50:53 +0530

    [Ashutosh]Added:Using ER diagram

--- src/main/java/com/dbdemo/EmployeePayrollData.java
+++ src/main/java/com/dbdemo/EmployeePayrollData.java
@@ -9,6 +9,8 @@ public class EmployeePayrollData {
 	LocalDate start;
 	 String gender;
 	String department;
+	String phone;
+	String address;
 	public int getId() {
 		return id;
 	}
@@ -49,7 +51,14 @@ public class EmployeePayrollData {
 		this.gender=gender;
 		this.department=department;
 	}
-
+	public EmployeePayrollData(int id, String name, double salary, LocalDate startDate,String gender,String department,String phone,String address) {
+		this(id, name, salary);
+		this.start = startDate;
+		this.gender=gender;
+		this.department=department;
+		this.phone=phone;
+		this.address=address;
+	}
 	public LocalDate getStart() {
 		return start;
 	}
