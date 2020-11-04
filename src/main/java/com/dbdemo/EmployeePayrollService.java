2fac0c8c0ef150e3feb28cb7cf18b7ba72397fcb
ASHUTOSH <ashutosh@192.168.1.6>
2020-11-04 19:40:01 +0530

    [Ashutosh]Added:Delete Employee

--- src/main/java/com/dbdemo/EmployeePayrollService.java
+++ src/main/java/com/dbdemo/EmployeePayrollService.java
@@ -198,5 +198,12 @@ public class EmployeePayrollService {
 		if (employeePayrollData != null)
 			employeePayrollData.salary = salary;
 	}
+	
+	public void deleteEmployeePayroll(String name, IOService ioService) {
+		if (ioService.equals(IOService.REST_IO)) {
+			EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+			employeePayrollList.remove(employeePayrollData);
+		}
+	}
 
 }
